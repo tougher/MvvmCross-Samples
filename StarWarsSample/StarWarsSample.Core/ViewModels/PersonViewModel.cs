cce412de6f502413b298efe2544c4a9e2d2ffe79
Christoffer Hoel <cho@trackman.dk>
2017-10-18 14:39:26 +0200

    Updated to 5.3 and fixed some breaking changes related to the navigation.

--- StarWarsSample/StarWarsSample.Core/ViewModels/PersonViewModel.cs
+++ StarWarsSample/StarWarsSample.Core/ViewModels/PersonViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Acr.UserDialogs;
+using MvvmCross.Core.Navigation;
 using MvvmCross.Core.ViewModels;
 using StarWarsSample.Core.Models;
 using StarWarsSample.Core.MvxInteraction;
@@ -7,72 +8,72 @@ using StarWarsSample.Core.ViewModelResults;
 
 namespace StarWarsSample.Core.ViewModels
 {
-    public class PersonViewModel : BaseViewModel<Person, DestructionResult<Person>>
-    {
-        private readonly IUserDialogs _userDialogs;
+	public class PersonViewModel : BaseViewModel<Person, DestructionResult<Person>>
+	{
+		private readonly IUserDialogs _userDialogs;
+		private readonly IMvxNavigationService _navigationService;
 
-        public PersonViewModel(
-            IUserDialogs userDialogs)
-        {
-            _userDialogs = userDialogs;
+		public PersonViewModel(
+			IUserDialogs userDialogs, IMvxNavigationService navigationService)
+		{
+			_userDialogs = userDialogs;
+			_navigationService = navigationService;
 
-            DestroyPersonCommand = new MvxAsyncCommand(DestroyPerson);
-        }
+			DestroyPersonCommand = new MvxAsyncCommand(DestroyPerson);
+		}
 
-        // MvvmCross Lifecycle
-        public override Task Initialize(Person parameter)
-        {
-            Person = parameter;
+		// MvvmCross Lifecycle
+		public override void Prepare(Person parameter)
+		{
+			Person = parameter;
+		}
 
-            return Task.FromResult(0);
-        }
+		// MVVM Properties
+		private Person _person;
+		public Person Person
+		{
+			get
+			{
+				return _person;
+			}
+			set
+			{
+				_person = value;
+				RaisePropertyChanged(() => Person);
+			}
+		}
 
-        // MVVM Properties
-        private Person _person;
-        public Person Person
-        {
-            get
-            {
-                return _person;
-            }
-            set
-            {
-                _person = value;
-                RaisePropertyChanged(() => Person);
-            }
-        }
+		public MvxInteraction<DestructionAction> Interaction { get; set; } = new MvxInteraction<DestructionAction>();
 
-        public MvxInteraction<DestructionAction> Interaction { get; set; } = new MvxInteraction<DestructionAction>();
+		// MVVM Commands
+		public IMvxCommand DestroyPersonCommand { get; private set; }
 
-        // MVVM Commands
-        public IMvxCommand DestroyPersonCommand { get; private set; }
+		// Private methods
 
-        // Private methods
+		private async Task DestroyPerson()
+		{
+			var destroy = await _userDialogs.ConfirmAsync(new ConfirmConfig
+			{
+				Title = "Destroy Person",
+				Message = "Sir, are you sure you want to destroy this person?",
+				OkText = "YES",
+				CancelText = "No"
+			});
 
-        private async Task DestroyPerson()
-        {
-            var destroy = await _userDialogs.ConfirmAsync(new ConfirmConfig
-            {
-                Title = "Destroy Person",
-                Message = "Sir, are you sure you want to destroy this person?",
-                OkText = "YES",
-                CancelText = "No"
-            });
+			if (!destroy)
+				return;
 
-            if (!destroy)
-                return;
+			var request = new DestructionAction
+			{
+				OnDestroyed = () => _navigationService.Close(this,
+					new DestructionResult<Person>
+					{
+						Entity = Person,
+						Destroyed = true
+					})
+			};
 
-            var request = new DestructionAction
-            {
-                OnDestroyed = () => Close(
-                    new DestructionResult<Person>
-                    {
-                        Entity = Person,
-                        Destroyed = true
-                    })
-            };
-
-            Interaction.Raise(request);
-        }
-    }
+			Interaction.Raise(request);
+		}
+	}
 }
--- StarWarsSample/StarWarsSample.Core/ViewModels/PlanetViewModel.cs
+++ StarWarsSample/StarWarsSample.Core/ViewModels/PlanetViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Acr.UserDialogs;
+using MvvmCross.Core.Navigation;
 using MvvmCross.Core.ViewModels;
 using StarWarsSample.Core.Models;
 using StarWarsSample.Core.MvxInteraction;
@@ -8,66 +9,66 @@ using StarWarsSample.Core.ViewModelResults;
 
 namespace StarWarsSample.Core.ViewModels
 {
-    public class PlanetViewModel : BaseViewModel<Planet, DestructionResult<Planet>>
-    {
-        private readonly IUserDialogs _userDialogs;
+	public class PlanetViewModel : BaseViewModel<Planet, DestructionResult<Planet>>
+	{
+		private readonly IUserDialogs _userDialogs;
+		private readonly IMvxNavigationService _navigationService;
 
-        public PlanetViewModel(
-            IUserDialogs userDialogs)
-        {
-            _userDialogs = userDialogs;
+		public PlanetViewModel(
+			IUserDialogs userDialogs, IMvxNavigationService navigationService)
+		{
+			_userDialogs = userDialogs;
+			_navigationService = navigationService;
 
-            DestroyPlanetCommand = new MvxAsyncCommand(DestroyPlanet);
-        }
+			DestroyPlanetCommand = new MvxAsyncCommand(DestroyPlanet);
+		}
 
-        // MvvmCross Lifecycle
-        public override Task Initialize(Planet parameter)
-        {
-            Planet = parameter;
+		// MvvmCross Lifecycle
+		public override void Prepare(Planet parameter)
+		{
+			Planet = parameter;
+		}
 
-            return Task.FromResult(0);
-        }
+		// MVVM Properties
+		private Planet _planet;
+		public Planet Planet
+		{
+			get
+			{
+				return _planet;
+			}
+			set
+			{
+				_planet = value;
+				RaisePropertyChanged(() => Planet);
+			}
+		}
 
-        // MVVM Properties
-        private Planet _planet;
-        public Planet Planet
-        {
-            get
-            {
-                return _planet;
-            }
-            set
-            {
-                _planet = value;
-                RaisePropertyChanged(() => Planet);
-            }
-        }
+		public MvxInteraction<DestructionAction> Interaction { get; set; } = new MvxInteraction<DestructionAction>();
 
-        public MvxInteraction<DestructionAction> Interaction { get; set; } = new MvxInteraction<DestructionAction>();
+		// MVVM Commands
+		public IMvxCommand DestroyPlanetCommand { get; private set; }
 
-        // MVVM Commands
-        public IMvxCommand DestroyPlanetCommand { get; private set; }
+		// Private methods
+		private async Task DestroyPlanet()
+		{
+			var destroy = await _userDialogs.ConfirmAsync(new ConfirmConfig
+			{
+				Title = "Destroy Planet",
+				Message = "Sir, are you sure you want to destroy this planet?",
+				OkText = "YES",
+				CancelText = "No"
+			});
 
-        // Private methods
-        private async Task DestroyPlanet()
-        {
-            var destroy = await _userDialogs.ConfirmAsync(new ConfirmConfig
-            {
-                Title = "Destroy Planet",
-                Message = "Sir, are you sure you want to destroy this planet?",
-                OkText = "YES",
-                CancelText = "No"
-            });
+			if (!destroy)
+				return;
 
-            if (!destroy)
-                return;
+			var request = new DestructionAction
+			{
+				OnDestroyed = () => _navigationService.Close(this, new DestructionResult<Planet> { Entity = Planet, Destroyed = true })
+			};
 
-            var request = new DestructionAction
-            {
-                OnDestroyed = () => Close(new DestructionResult<Planet> { Entity = Planet, Destroyed = true })
-            };
-
-            Interaction.Raise(request);
-        }
-    }
+			Interaction.Raise(request);
+		}
+	}
 }
